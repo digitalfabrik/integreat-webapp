cca22774114bd5b9390dbbf2ef6f93ea97117ad1
Steffen Kleinle <steffen.kleinle@web.de>
2018-04-20 14:37:46 +0200

    WEBAPP-259: Fix design if there are no thumbnails

--- src/routes/categories/components/CategoryListItem.styles.js
+++ src/routes/categories/components/CategoryListItem.styles.js
@@ -22,7 +22,7 @@ export const SubCategory = styled.div`
   text-align: end;
   
   & > * {
-    width: calc(100% - 66px);
+    width: calc(100% - 50px);
     text-align: start;
     
     @media ${props => props.theme.dimensions.smallViewport} {
@@ -34,26 +34,31 @@ export const SubCategory = styled.div`
 export const SubCategoryThumbnail = CategoryThumbnail.extend`
   width: 26px;
   height: 26px;
-  padding: 5px 0;
+  padding: 5px 10px;
   
   @media ${props => props.theme.dimensions.smallViewport} {
     padding: 5px 15px;
   }  
 `
-export const SubCategoryThumbnailDiv = SubCategoryThumbnail.withComponent('div')
+export const SubCategoryThumbnailDiv = SubCategoryThumbnail.extend`
+  width: 15px;
+  
+  @media ${props => props.theme.dimensions.smallViewport} {
+    width: 40px;
+  }  
+`.withComponent('div')
 
 export const CategoryCaption = styled(Highlighter)`
   height: 100%;
-  min-width: 1px; /* needed to enabled line breaks for to long words, exact value doesn't matter, @Max: DO NOT CHANGE */
+  min-width: 1px; /* needed to enable line breaks for to long words, exact value doesn't matter, @Max: DO NOT CHANGE */
   flex-grow: 1;
-  margin-left: 10px;
   padding: 15px 0;
   border-bottom: 2px solid ${props => props.theme.colors.themeColor};
   word-wrap: break-word;
 `
 
 export const SubCategoryCaption = CategoryCaption.extend`
-  padding: 10px 0 10px 15px;
+  padding: 8px 0;
   border-bottom: 1px solid ${props => props.theme.colors.themeColor};
 `
 
